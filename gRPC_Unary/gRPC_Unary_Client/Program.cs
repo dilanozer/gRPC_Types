52de51bbebb82595ec88a2582759ddd0aef4c92e
Dilan ÖZER <dilanozer@Dilans-MacBook-Air.local>
2024-08-08 22:43:28 +0300

    unary rpc ile client-server iletisimi

---
+++ gRPC_Unary/gRPC_Unary_Client/Program.cs
@@ -0,0 +1,14 @@
+﻿using Grpc.Net.Client;
+using gRPC_Unary_Client.Protos;
+
+var channel = GrpcChannel.ForAddress("http://localhost:5137");
+var messageClient = new Message.MessageClient(channel);
+
+MessageResponse response = await messageClient.SendMessageAsync(new MessageRequest
+{
+    Message = "Merhaba",
+    Name = "Dilan"
+});
+
+Console.WriteLine(response.Message);
+
---
+++ gRPC_Unary/gRPC_Unary_Server/Services/MessageService.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Grpc.Core;
+using gRPC_Unary_Server.Protos;
+
+namespace gRPC_Unary_Server.Services
+{
+	public class MessageService : Message.MessageBase
+	{
+        private readonly ILogger<MessageService> _logger;
+
+        public MessageService(ILogger<MessageService> logger)
+        {
+            _logger = logger;
+        }
+
+        public override async Task<MessageResponse> SendMessage(MessageRequest request, ServerCallContext context)
+        {
+            Console.WriteLine($"Message: {request.Message} | Name: {request.Name} ");
+
+            return new MessageResponse
+            {
+                Message = "Mesaj başarıyla alınmıştır..."
+            };
+        }
+    }
+}
+
