3a89e6da318bd17d81ee808cec6478016addb0b2
Dilan ÖZER <dilanozer@Dilans-MacBook-Air.local>
2024-08-09 00:23:56 +0300

    bi-directional streaming rpc türü ile client-server iletisimi

---
+++ gRPC_BiDirectionalStreaming/gRPC_BiDirectionalStreaming_Client/Program.cs
@@ -0,0 +1,33 @@
+﻿using Grpc.Net.Client;
+using gRPC_BiDirectionalStreaming_Client.Protos;
+
+var channel = GrpcChannel.ForAddress("http://localhost:5150");
+var messageClient = new Message.MessageClient(channel);
+
+var request = messageClient.SendMessage();
+
+CancellationTokenSource cancellationTokenSource = new();
+
+var task1 = Task.Run(async () =>
+{
+    for (int i = 0; i < 10; i++)
+    {
+        await Task.Delay(1000);
+
+        request.RequestStream.WriteAsync(new MessageRequest
+        {
+            Name = "Dilan",
+            Message = "Mesaj " + i
+        });
+    }
+});
+
+while (await request.ResponseStream.MoveNext(cancellationTokenSource.Token))
+{
+    Console.WriteLine(request.ResponseStream.Current.Message);
+
+}
+
+await task1;
+
+await request.RequestStream.CompleteAsync();
---
+++ gRPC_BiDirectionalStreaming/gRPC_BiDirectionalStreaming_Server/Services/MessageService.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Grpc.Core;
+using gRPC_BiDirectionalStreaming_Server.Protos;
+
+namespace gRPC_BiDirectionalStreaming_Server.Services
+{
+	public class MessageService : Message.MessageBase
+	{
+		private readonly ILogger<MessageService> _logger;
+		public MessageService(ILogger<MessageService> logger)
+		{
+			_logger = logger;
+		}
+
+        public override async Task SendMessage(IAsyncStreamReader<MessageRequest> requestStream, IServerStreamWriter<MessageResponse> responseStream, ServerCallContext context)
+        {
+			var task1 = Task.Run(async () =>
+			{
+				while (await requestStream.MoveNext(context.CancellationToken))
+				{
+					Console.WriteLine($"Message: {requestStream.Current.Message} | Name: {requestStream.Current.Name}");
+				}
+			});
+
+			for (int i = 0; i < 10; i++)
+			{
+				await Task.Delay(1000);
+
+				await responseStream.WriteAsync(new MessageResponse
+				{
+					Message = "Mesaj " + i
+				});
+			}
+
+            await task1;
+        }
+    }
+}
+
