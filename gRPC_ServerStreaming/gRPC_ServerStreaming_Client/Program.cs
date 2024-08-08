41ef02c917ef8f36b14ccb030c3bf4dad9b0989c
Dilan ÖZER <dilanozer@Dilans-MacBook-Air.local>
2024-08-08 23:25:33 +0300

    server streaming rpc türü ile client-server iletisimi

---
+++ gRPC_ServerStreaming/gRPC_ServerStreaming_Client/Program.cs
@@ -0,0 +1,19 @@
+﻿using Grpc.Net.Client;
+using gRPC_ServerStreaming_Client.Protos;
+
+var channel = GrpcChannel.ForAddress("http://localhost:5293");
+var messageClient = new Message.MessageClient(channel);
+
+var response = messageClient.SendMessage(new MessageRequest
+{
+    Message = "Merhaba",
+    Name = "Dilan"
+});
+
+CancellationTokenSource cancellationTokenSource = new();
+
+while (await response.ResponseStream.MoveNext(cancellationTokenSource.Token))
+{
+    Console.WriteLine(response.ResponseStream.Current.Message);
+}
+
---
+++ gRPC_ServerStreaming/gRPC_ServerStreming_Server/Services/MessageService.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Grpc.Core;
+using gRPC_ServerStreaming_Server.Protos;
+
+namespace gRPC_ServerStreaming_Server.Services
+{
+    public class MessageService : Message.MessageBase
+	{
+        private readonly ILogger<MessageService> _logger;
+
+        public MessageService(ILogger<MessageService> logger)
+		{
+			_logger = logger;
+		}
+
+        public override async Task SendMessage(MessageRequest request, IServerStreamWriter<MessageResponse> responseStream, ServerCallContext context)
+        {
+            Console.WriteLine($"Message: {request.Message} | Name: {request.Name}");
+
+            for (int i = 0; i < 10; i++)
+            {
+                await Task.Delay(1000);
+
+                await responseStream.WriteAsync(new MessageResponse
+                {
+                    Message = "Merhaba " + i
+                });
+            }
+            
+        }
+    }
+}
+
